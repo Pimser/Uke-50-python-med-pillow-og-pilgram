dda44bbb428c15b310b3b84c1c07b29478d9fddd
Pimser <142997610+Pimser@users.noreply.github.com>
2023-12-20 18:12:05 +0100

    Create Repetisjonsoppgaver.py

---
+++ Repetisjonsoppgaver.py
@@ -0,0 +1,55 @@
+# oppg 1 a)
+import time
+#x = 0
+#hundre = 0
+
+
+#for x in range(10):
+    #x += 1
+    #time.sleep(0.1)
+    #print(x)
+
+#oppg 1 b)
+
+#for hundre in range(100):
+    #hundre += 1
+    #time.sleep(0.1)
+    #print(("+"), hundre)
+
+#oppg 2
+
+#sum = 0
+#while sum < 30:
+ #   sum += 2
+  #  print(sum)
+
+#oppg 3 a)
+
+def Legg_sammen():
+    input("skriv inn et tall -->")
+    input("skirv inn enda et tall her -->")
+    
+
+
+
+
+#Elgardo koder sammen med oss (tirsdag time 2 uke 50)
+#def pizza():
+ #mel = 400
+ #ost = 300
+# ananas = 0
+ #tomat = 250
+ #oregano = 5
+ #olje = 100
+ #gjær = 5
+ #vann = 150
+ #result =0
+ #lista_ingredienser = [mel, ost, ananas, tomat, oregano, olje, gjær, vann]
+ #for x in range(len(lista_ingredienser)):
+  #result += lista_ingredienser[x]
+  #print(result)
+
+#pizza()
+
+
+#
